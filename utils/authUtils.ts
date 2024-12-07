b626f8b006392463054bec685b498091bf5218dd
Ian Mazurik <mazurikian@protonmail.com>
2024-12-07 03:29:57 -0300

    Update authUtils.ts

--- utils/authUtils.ts
+++ utils/authUtils.ts
@@ -1,58 +1,31 @@
-import crypto from "crypto";
+import crypto from 'crypto';
 
-const NONCE_KEY = "vicopx7dqu06emacgpnpy8j8zwhduwlh";
-const AUTH_KEY = "9u7qab84rpc16gvk";
+// Constants for encryption keys
+const NONCE_KEY = 'vicopx7dqu06emacgpnpy8j8zwhduwlh';
+const AUTH_KEY = '9u7qab84rpc16gvk';
 
+// Decrypt nonce
 export const decryptNonce = (nonceEncrypted: string): string => {
-  const nonceDecipher = crypto.createDecipheriv(
-    "aes-256-cbc",
-    NONCE_KEY,
-    NONCE_KEY.slice(0, 16)
-  );
-
-  return Buffer.concat([
-    nonceDecipher.update(nonceEncrypted, "base64"),
-    nonceDecipher.final(),
-  ]).toString("utf-8");
+  const nonceDecipher = crypto.createDecipheriv('aes-256-cbc', NONCE_KEY, NONCE_KEY.slice(0, 16));
+  return Buffer.concat([nonceDecipher.update(nonceEncrypted, 'base64'), nonceDecipher.final()]).toString('utf-8');
 };
 
+// Generate Authorization header
 export const getAuthorization = (nonceDecrypted: string): string => {
-  let key = "";
-
-  for (let i = 0; i < 16; i += 1) {
+  let key = '';
+  for (let i = 0; i < 16; i++) {
     const nonceChar = nonceDecrypted.charCodeAt(i);
     key += NONCE_KEY[nonceChar % 16];
   }
-
   key += AUTH_KEY;
-
-  const authCipher = crypto.createCipheriv(
-    "aes-256-cbc",
-    key,
-    key.slice(0, 16)
-  );
-
-  return Buffer.concat([
-    authCipher.update(nonceDecrypted, "utf8"),
-    authCipher.final(),
-  ]).toString("base64");
+  const authCipher = crypto.createCipheriv('aes-256-cbc', key, key.slice(0, 16));
+  return Buffer.concat([authCipher.update(nonceDecrypted, 'utf8'), authCipher.final()]).toString('base64');
 };
 
-export const handleAuthRotation = (
-  responseHeaders: Record<string, string>
-): {
-  Authorization: string;
-  nonce: { decrypted: string; encrypted: string };
-} => {
+// Handle authentication rotation and session management
+export const handleAuthRotation = (responseHeaders: Record<string, string>) => {
   const { nonce } = responseHeaders;
   const nonceDecrypted = decryptNonce(nonce);
   const authorization = getAuthorization(nonceDecrypted);
-
-  return {
-    Authorization: `FUS nonce="${nonce}", signature="${authorization}", nc="", type="", realm="", newauth="1"`,
-    nonce: {
-      decrypted: nonceDecrypted,
-      encrypted: nonce,
-    },
-  };
+  return { Authorization: `FUS nonce="${nonce}", signature="${authorization}", nc="", type="", realm="", newauth="1"`, nonce: { decrypted: nonceDecrypted, encrypted: nonce } };
 };
