5a8ba603b4c9fde3153df9b79666022e292cfc21
Ian Mazurik <mazurikian@protonmail.com>
2024-12-07 04:48:22 -0300

    Update FUSMsg.ts

--- types/FUSMsg.ts
+++ types/FUSMsg.ts
@@ -1,39 +1,44 @@
+// Definición de la interfaz FUSMsg
 export interface FUSMsg {
   FUSMsg: {
+    // Cabecera del mensaje
     FUSHdr: {
-      ProtoVer: string;
+      ProtoVer: string; // Versión del protocolo
     };
+
+    // Cuerpo del mensaje
     FUSBody: {
       Put: {
+        // Propiedades opcionales que se pueden incluir en el cuerpo del mensaje
         ACCESS_MODE?: {
-          Data: number;
+          Data: number; // Modo de acceso
         };
         BINARY_FILE_NAME?: {
-          Data: string;
+          Data: string; // Nombre del archivo binario
         };
         BINARY_NATURE?: {
-          Data: number;
+          Data: number; // Naturaleza del binario
         };
         CLIENT_PRODUCT?: {
-          Data: string;
+          Data: string; // Producto cliente
         };
         CLIENT_VERSION?: {
-          Data: string;
+          Data: string; // Versión del cliente
         };
         DEVICE_IMEI_PUSH?: {
-          Data: string;
+          Data: string; // IMEI del dispositivo
         };
         DEVICE_FW_VERSION?: {
-          Data: string;
+          Data: string; // Versión del firmware del dispositivo
         };
         DEVICE_LOCAL_CODE?: {
-          Data: string;
+          Data: string; // Código regional del dispositivo
         };
         DEVICE_MODEL_NAME?: {
-          Data: string;
+          Data: string; // Nombre del modelo del dispositivo
         };
         LOGIC_CHECK?: {
-          Data: string;
+          Data: string; // Verificación lógica
         };
       };
     };
