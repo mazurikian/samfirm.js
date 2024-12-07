1f33403f5a01414b5c13ceaca7c08011ebfc55a9
Ian Mazurik <mazurikian@protonmail.com>
2024-12-07 04:28:13 -0300

    Update FUSMsg.ts

--- types/FUSMsg.ts
+++ types/FUSMsg.ts
@@ -1,41 +1,31 @@
 export interface FUSMsg {
   FUSMsg: {
-    FUSHdr: {
-      ProtoVer: string;
-    };
-    FUSBody: {
-      Put: {
-        ACCESS_MODE?: {
-          Data: number;
-        };
-        BINARY_FILE_NAME?: {
-          Data: string;
-        };
-        BINARY_NATURE?: {
-          Data: number;
-        };
-        CLIENT_PRODUCT?: {
-          Data: string;
-        };
-        CLIENT_VERSION?: {
-          Data: string;
-        };
-        DEVICE_IMEI_PUSH?: {
-          Data: string;
-        };
-        DEVICE_FW_VERSION?: {
-          Data: string;
-        };
-        DEVICE_LOCAL_CODE?: {
-          Data: string;
-        };
-        DEVICE_MODEL_NAME?: {
-          Data: string;
-        };
-        LOGIC_CHECK?: {
-          Data: string;
-        };
-      };
-    };
+    FUSHdr: FUSHdr;
+    FUSBody: FUSBody;
   };
+}
+
+export interface FUSHdr {
+  ProtoVer: string;
+}
+
+export interface FUSBody {
+  Put: FUSPut;
+}
+
+export interface FUSPut {
+  ACCESS_MODE?: DataField<number>;
+  BINARY_FILE_NAME?: DataField<string>;
+  BINARY_NATURE?: DataField<number>;
+  CLIENT_PRODUCT?: DataField<string>;
+  CLIENT_VERSION?: DataField<string>;
+  DEVICE_IMEI_PUSH?: DataField<string>;
+  DEVICE_FW_VERSION?: DataField<string>;
+  DEVICE_LOCAL_CODE?: DataField<string>;
+  DEVICE_MODEL_NAME?: DataField<string>;
+  LOGIC_CHECK?: DataField<string>;
+}
+
+export interface DataField<T> {
+  Data: T;
 }
